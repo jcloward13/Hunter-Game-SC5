cd55449abf0a042c8db11e54f8529ed885f1dd42
Jacob Cloward <jacobcloward@yahoo.com>
2021-06-16 22:21:47 -0600

    Updated Rabbit.py with the Rabbit class

--- hunter/game/rabbit.py
+++ hunter/game/rabbit.py
@@ -1,3 +1,13 @@
 import random
 
-# TODO: Define the Rabbit class here
+class Rabbit:
+    def __init__(self):
+        self.distance = range(1,1001)
+        self.location = random.randint(1,1001)
+        print(self.location)
+
+    def get_hint(self):
+        pass
+
+    def watch(self,location):
+        pass
