20345796fdebce6ea8cf65e5f98f946406a18070
Jacob Cloward <jacobcloward@yahoo.com>
2021-06-17 15:05:41 -0600

    updated Rabbit.py

--- hunter/game/rabbit.py
+++ hunter/game/rabbit.py
@@ -1,12 +1,21 @@
 import random
 
+
 class Rabbit:
     def __init__(self):
-        self.distance = range(1,1001)
-        self.location = random.randint(1,1001)
+        self.distance = [0, 0]
+        self.location = random.randint(1, 1001)
 
     def get_hint(self):
-        pass
+        hint = "(-.-) Maybe I'll take a nap."
+        if self.distance[-1] == 0:
+            hint = "(;.;) You found me!"
+        elif self.distance[-1] > self.distance[-2]:
+            hint = "(^.^) Getting colder!"
+        elif self.distance[-1] < self.distance[-2]:
+            hint = "(>.<) Getting warmer!"
+        return hint
 
-    def watch(self,location):
-        pass
+    def watch(self, location):
+        distance = abs(self.location - location)
+        self.distance.append(distance)
